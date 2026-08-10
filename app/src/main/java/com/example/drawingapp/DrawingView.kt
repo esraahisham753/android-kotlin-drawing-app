fa74b13e4f25d386b36c36929572b4b72b703ede
Esraa Abduallah <esraahisham753@gmail.com>
2026-08-10 17:15:45 +0300

    Feat: Fix the crashing app, add checks and persist drawing paths.

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -1,12 +1,15 @@
 package com.example.drawingapp
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import androidx.core.graphics.createBitmap
 
 class DrawingView(context: Context, attr: AttributeSet): View(context, attr) {
     private lateinit var drawingPath: FingerPath
@@ -15,14 +18,95 @@ class DrawingView(context: Context, attr: AttributeSet): View(context, attr) {
     private var brushThickness = 0f
     private lateinit var canvas: Canvas
     private lateinit var canvasPaint: Paint
+    private lateinit var canvasBitmap: Bitmap
 
     init {
         setupDrawing()
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        if (w > 0 && h > 0) {
+            canvasBitmap = createBitmap(w, h)
+            canvas = Canvas(canvasBitmap)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        if (::canvasBitmap.isInitialized) {
+            canvas.drawBitmap(canvasBitmap, 0f, 0f, canvasPaint)
+
+            if (!drawingPath.isEmpty) {
+                drawingPaint.apply {
+                    color = drawingPath.color
+                    strokeWidth = drawingPath.brushThickness
+                }
+
+                canvas.drawPath(drawingPath, drawingPaint)
+            }
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                drawingPath.apply {
+                    color = color
+                    brushThickness = brushThickness
+                }
+
+                drawingPath.reset()
+
+                if (touchX != null && touchY != null) {
+                    drawingPath.moveTo(touchX, touchY)
+                }
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null && touchY != null) {
+                    drawingPath.lineTo(touchX, touchY)
+                }
+            }
+
+            MotionEvent.ACTION_UP -> {
+                drawingPath.apply {
+                    color = color
+                    brushThickness = brushThickness
+                }
+
+                drawingPaint.apply {
+                    color = drawingPath.color
+                    brushThickness = drawingPath.brushThickness
+                }
+
+                canvas.drawPath(drawingPath, drawingPaint)
+                drawingPath.reset()
+
+                performClick()
+            }
+
+            else -> return false
+        }
+
+        invalidate()
+
+        return true
+    }
+
+    override fun performClick(): Boolean {
+        return super.performClick()
+    }
+
     fun setupDrawing() {
         brushThickness = 20f
         drawingPath = FingerPath(color, brushThickness)
+        drawingPaint = Paint()
 
         drawingPaint.apply {
             style = Paint.Style.STROKE
@@ -30,9 +114,11 @@ class DrawingView(context: Context, attr: AttributeSet): View(context, attr) {
             strokeCap = Paint.Cap.ROUND
             strokeJoin = Paint.Join.ROUND
         }
+
+        canvasPaint = Paint(Paint.DITHER_FLAG)
     }
 
-    internal inner class FingerPath(val color: Int, val brushThickness: Float): Path() {
+    internal inner class FingerPath(var color: Int, var brushThickness: Float): Path() {
 
     }
 }
